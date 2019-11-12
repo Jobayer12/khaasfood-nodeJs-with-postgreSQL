755c263d3678ee51f77f8c68740d098890cddea4
jobayer12 <jobayer.cse43.diu@gmail.com>
2019-11-12 19:06:09 +0600

    index page update

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ app.get("/products/:category", product.category);
 app.get("/products/:category/:sub_category", product.sub_category);
 app.get("/search", searchQuery.search);
 
+console.log("app running");
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 // select uid,name from public.product  where name like '%organic eg%' union all select uid, name from public.popular where name like '%organic eg%' union all select uid, name from public.product  where name like '%organic eg%'
